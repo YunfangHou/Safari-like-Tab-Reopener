4a0fdad6bcc609a58e331cf48371726d42c7f7d0
YunfangHou <Yunfang.Hou2001@gmail.com>
2024-04-03 01:20:53 +0100

    Fix bug: still reopen in github code editing area

--- package for Chromium/content.js
+++ package for Chromium/content.js
@@ -1,8 +1,8 @@
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     if ((event.ctrlKey || event.metaKey) && event.key === 'z') {
-        if (!(document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA')) {
+        if (!(document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA' || document.activeElement.getAttribute("role") === 'textbox')) {
             event.preventDefault();
-            chrome.runtime.sendMessage({command: "reopen_last_tab"});
+            chrome.runtime.sendMessage({ command: "reopen_last_tab" });
         }
     }
 });
--- package for Firefox/content.js
+++ package for Firefox/content.js
@@ -1,8 +1,8 @@
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     if ((event.ctrlKey || event.metaKey) && event.key === 'z') {
-        if (!(document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA')) {
+        if (!(document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA' || document.activeElement.getAttribute("role") === 'textbox')) {
             event.preventDefault();
-            browser.runtime.sendMessage({command: "reopen_last_tab"});
+            browser.runtime.sendMessage({ command: "reopen_last_tab" });
         }
     }
 });
